ac230c4b937f6facbdd16e2af38f3adfc03224b5
S-Hashi526 <s.hashimo.0526@gmail.com>
2022-06-11 13:22:10 +0000

    ユーザーリソース作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# cording: utf-8
+# coding: utf-8
 
 User.create!(name: "Sample User",
              email: "sample@email.com",
              password: "password",
-             password_confirmation: "password")
+             password_confirmation: "password",
+             admin: true)
              
 60.times do |n|
   name = Faker::Name.name
