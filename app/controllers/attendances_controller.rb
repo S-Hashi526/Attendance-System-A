f460e8c52c1c7c19b3e79a9cab76f9615592a29e
S-Hashi526 <s.hashimo.0526@gmail.com>
2022-07-10 13:14:13 +0000

    いったん戻し

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -30,7 +30,7 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-    ActiveRecord::Base.transaction do
+    ActiveRecord::Base.transaction do # トランザクションを開始。
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
         attendance.update_attributes!(item)
@@ -38,12 +38,13 @@ class AttendancesController < ApplicationController
     end
     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
   
   private
+  
     # 1か月分の勤怠情報を扱います。
     def attendances_params
       params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
