6d824ad32e320f604e079bf31332d57140e8324b
S.hashimo0526 <s.hashimo.0526@gmail.com>
2022-09-11 22:41:55 +0900

    上長ユーザー作成

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,11 @@ ActiveRecord::Schema.define(version: 20220715131206) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-09-06 23:00:00"
-    t.datetime "work_time", default: "2022-09-06 22:30:00"
+    t.datetime "basic_time", default: "2022-09-10 22:30:00"
+    t.datetime "work_time", default: "2022-09-10 22:30:00"
+    t.integer "employee_number"
+    t.integer "card_id"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,50 @@
 # coding: utf-8
 
-User.create!(name: "Sample User",
-             email: "sample@email.com",
-             password: "password",
-             password_confirmation: "password",
-             admin: true)
-             
-60.times do |n|
+User.create!(
+  [
+    {
+      name: "管理者",
+      email: "sample@email.com",
+      employee_number: "0001",
+      card_id: "0001",
+      password: "password",
+      password_confirmation: "password",
+      admin: true
+    },
+    {
+      name: "上長A",
+      email: "superior-a@email.com",
+      employee_number: "1001",
+      card_id: "1001",
+      password: "password",
+      password_confirmation: "password",
+      admin: false,
+      superior: true
+    },
+    {
+      name: "上長B",
+      email: "superior-b@email.com",
+      employee_number: "1002",
+      card_id: "1002",
+      password: "password",
+      password_confirmation: "password",
+      admin: false,
+      superior: true
+    },
+  ]
+)  
+  
+3.times do |n|
   name = Faker::Name.name
   email = "sample-#{n+1}@email.com"
   password = "password"
   User.create!(name: name,
                email: email,
+               employee_number: n+1101,
+               card_id: n+1101,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               admin: false,
+               superior: false
+              )
 end
