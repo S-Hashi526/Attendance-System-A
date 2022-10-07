9c284f3082ea53bad9991fd5c3ce93950d3c913f
S.hashimo0526 <s.hashimo.0526@gmail.com>
2022-10-07 22:56:05 +0900

    sessions_controllerを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      # ログイン
       log_in user
+
+      # ログイン情報を記憶するかしないか
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+
+      # 管理者であればユーザー一覧画面に遷移可
+      if current_user.admin?
+        redirect_to users_url
+      else
+        redirect_to current_user
+      end
+
+      # ユーザー情報ページにリダイレクト
+      # redirect_back_or user
+
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
@@ -16,8 +29,10 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    # ログイン中の場合のみログアウト可能
     log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
+  
 end
